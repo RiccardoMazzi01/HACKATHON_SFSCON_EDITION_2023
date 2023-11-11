5c605bba1c41733da6d45c94e01d60431697c196
Riccardo Mazzi <80252053+RiccardoMazzi01@users.noreply.github.com>
2023-11-11 13:32:42 +0100

    a

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Temperature)
-admin.site.register(Watt)
-admin.site.register(Sector)
-admin.site.register(FirestoreData)
